8a9caed05cdd65ce13b2a3682b54139d2d9fdb6f
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-11 18:27:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,16 @@ def batch_badge_creator(name)
 end
 
 def aissign_rooms(data)
+  array =[]
+  room = 1
+    if data.length > 0
+      data.each do |name|
+      array << "Hello, #{name}! You'll be assigned to #{room}."
+      room +=1
+      end
+      return array
+    end
+end
   
 
 
